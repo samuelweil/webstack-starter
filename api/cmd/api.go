0d8351ce165f9e7b505d83b18ee04e6cfd26e003
Sam Weil <s.weil9391@gmail.com>
2021-11-14 12:23:59 -0600

    Add an about endpoint to the API
    This endpoint doesn't do anything but is useful for testing setup

--- api/cmd/api.go
+++ api/cmd/api.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
+const VERSION = "nightly"
+
+type About struct {
+	Version string `json:"version"`
+}
+
+var aboutInfo About = About{
+	Version: "nightly",
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Printf("Starting API (%s)\n", VERSION)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", aboutHandler)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	writeJSON(aboutInfo, w)
+}
+
+func writeJSON(i interface{}, w http.ResponseWriter) error {
+	resultBytes, err := json.Marshal(i)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(resultBytes)
+	return err
 }
